ce42a8eaaa0ab5f3b276a99f87082c2b5ea57878
Kurokodairu <jsvendsli@gmail.com>
2019-03-23 22:39:19 +0100

    NavBar and Components

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express()
 const path = require("path");
 const fs = require('fs');
-
-const port = 8080;
+var http = require('http')
+const port = 8081;
 
 const url = require("url");
 
+// Using EJS as Template Files. https://ejs.co 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
+app.set('trust proxy', true);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
@@ -22,9 +24,10 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
     res.render(pathname, purl.query);
 });
 
-
 app.get('/api/testing', (req, res) => {
     res.status(200).send('Hello world!');
 });
 
 // https://github.com/DevonCrawford/Personal-Website TEMPLATE
+
+// J QUERY ??
