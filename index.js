d0468658614c5c4a5423e57e109c00de1e7569f8
Kurokodairu <jsvendsli@gmail.com>
2019-03-29 20:27:18 +0100

    Updated Port for hosting providers

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-const port = 80;
+const port = 3000;
 
 const url = require("url");
 
